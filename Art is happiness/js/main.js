62badaefc9d63248b209fe7a5475cfe14526fdd3
Aïman <47420032+Aiman75@users.noreply.github.com>
2021-03-11 15:56:01 +0100

    Update main.js

--- Art is happiness/js/main.js
+++ Art is happiness/js/main.js
@@ -1,44 +1,82 @@
-$(function() {
-    $('.scroll-down').click (function() {
-      $('html, body').animate({scrollTop: $('section.ok').offset().top }, 'slow');
-      return false;
-    });
-  });
-
-  ////////////////////// DEBUT CAROUSSEL ////////////////////////////////
-
-  // $(document).ready(function(){
-  //   $('.owl-carousel').owlCarousel({
-  //     autoplay:true,
-  //     loop:true
-  //   })
-  // });
-
-
-  var prev = document.querySelector('div.prev');
-var next = document.querySelector('div.next');
-var index = 0;
-var lis = document.querySelectorAll('ul.slider li');
-var count = lis.length;
-
-prev.addEventListener('click', function() {
-    lis[index].classList.remove('visible');
-    index--;
-    if (index < 0){
-        index = count-1;
-    }
-
-    lis[index].classList.add('visible');
-});
-
-next.addEventListener('click', function() {
-    lis[index].classList.remove('visible');
-    index++;
-    if (index > count-1){
-        index = 0;
-    }
-
-    lis[index].classList.add('visible');
-});
-
-///////////////////// FIN du caroussel //////////////////////////////////////////////////
+// Global var
+const $prevBtn = $('.prev-image')
+const $nextBtn = $('.next-image')
+const $carouselItems = $('.carousel-item')
+const $carouselPauseBtn = $('.carousel-pause-btn')
+ 
+let currentItemPosition = 0
+let carouselInterval
+ 
+// Funcs
+const goToNextSlide = () => {
+   if (currentItemPosition + 1 >=  $carouselItems.length) {
+      
+       const lastItem = `.item-${currentItemPosition}`
+ 
+       currentItemPosition = 0
+       const currentItem = `.item-${currentItemPosition}`
+      
+       setNodeAttributes(lastItem, currentItem)
+   } else {
+       currentItemPosition += 1
+       const lastItem = `.item-${currentItemPosition - 1}`
+       const currentItem = `.item-${currentItemPosition}`
+      
+       setNodeAttributes(lastItem, currentItem)
+   }
+}
+ 
+const goToPreviousSlide = () => {
+   if (currentItemPosition - 1 >=  0) {
+       currentItemPosition -= 1
+       const currentItem = `.item-${currentItemPosition}`
+       const lastItem = `.item-${currentItemPosition + 1}`
+ 
+       setNodeAttributes(lastItem, currentItem)
+   } else {
+       const lastItem = `.item-${currentItemPosition}`
+      
+       currentItemPosition = 2
+       const currentItem = `.item-${currentItemPosition}`
+      
+       setNodeAttributes(lastItem, currentItem)
+   }
+}
+ 
+ 
+const setNodeAttributes = (lastItem, currentItem) => {
+   $(lastItem).css('display', 'none')
+   $(currentItem).css('display', 'block')
+   $(lastItem).attr('aria-hidden', 'true')
+   $(currentItem).attr('aria-hidden', 'false')
+}
+ 
+ 
+// Events
+$prevBtn.click(function() {
+   goToPreviousSlide()
+})
+ 
+$nextBtn.click(function() {
+   goToNextSlide()
+})
+ 
+ 
+$(document).keydown(function(e) {
+   const keyCode = e.keyCode ? e.keyCode : e.which
+ 
+   if (keyCode === 39) {
+       goToNextSlide()
+   } else if (keyCode === 37) {
+       goToPreviousSlide()
+   }
+})
+ 
+$carouselPauseBtn.on('click', function() {
+   clearInterval(carouselInterval)
+})
+ 
+ 
+$(document).ready(function() {
+   carouselInterval = setInterval(() => goToNextSlide(), 3000)
+})
